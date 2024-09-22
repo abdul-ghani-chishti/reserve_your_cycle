abb72e621f777d989eb39342ca74212d3e3e4e89
Abdul Ghani Chishti <abdulghanichishti195@gmail.com>
2024-09-22 14:21:46 +0200

    just commit all routes

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,26 @@ Route::get('/', function () {
 });
 
 
-Route::prefix('login')->name('login.')->group(function () {
-    Route::get('', [LoginController::class,'index'])->name('index');
-
-    Route::get('admin_login', [LoginController::class,'admin_login'])->name('admin_login');
-    Route::post('admin_login_request', [LoginController::class,'admin_login'])->name('admin_login_request');
-    //    dd('k');
-    Route::get('user_login', [LoginController::class,'user_login'])->name('user_login');
-    Route::post('user_login_request', [LoginController::class,'user_login_request'])->name('user_login_request');
-});
-
-//admin routes
-Route::prefix('admin')->name('admin.')->group(function (){
-    Route::get('', [AdminDashboardController::class,'index'])->name('index');
-    Route::prefix('dashboard')->name('dashboard.')->group(function () {
-        Route::get('', [AdminDashboardController::class,'index'])->name('index');
-    });
-});
-
-//user routes
-Route::prefix('user')->name('user.')->group(function (){
-//    dd(6);
-    Route::get('', [UserDashboardController::class,'index'])->name('index');
-});
+//Route::prefix('login')->name('login.')->group(function () {
+//    Route::get('', [LoginController::class,'index'])->name('index');
+//
+//    Route::get('admin_login', [LoginController::class,'admin_login'])->name('admin_login');
+//    Route::post('admin_login_request', [LoginController::class,'admin_login'])->name('admin_login_request');
+//    //    dd('k');
+//    Route::get('user_login', [LoginController::class,'user_login'])->name('user_login');
+//    Route::post('user_login_request', [LoginController::class,'user_login_request'])->name('user_login_request');
+//});
+//
+////admin routes
+//Route::prefix('admin')->name('admin.')->group(function (){
+//    Route::get('', [AdminDashboardController::class,'index'])->name('index');
+//    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+//        Route::get('', [AdminDashboardController::class,'index'])->name('index');
+//    });
+//});
+//
+////user routes
+//Route::prefix('user')->name('user.')->group(function (){
+////    dd(6);
+//    Route::get('', [UserDashboardController::class,'index'])->name('index');
+//});
